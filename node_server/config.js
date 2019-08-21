57b6e20d82d1fbf4597c5b8dbb60424c7fb807cb
octobermom0103 <33580000+octobermom0103@users.noreply.github.com>
2019-08-21 19:57:46 -0400

    Update config.js

--- node_server/config.js
+++ node_server/config.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = {
-    email: '',
-    password: '',
-    smartThingsHubIP: '10.0.0.200',
+    email: 'octobermom0103@gmail.com',
+    password: 'u0783yM3CFDt',
+    smartThingsHubIP: '192.168.1.4',
     smartThingsAppId: '', //Optional: Can be used to restrict SmartThings app to listening for certain Sense Stream server
     autoReconnect: true, // Attempt to reconnect to sense service of dropped connections
     callbackPort: 9021 // Port used by the web server to receive settings changes from ST
